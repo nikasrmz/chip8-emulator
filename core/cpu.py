3b1850ca1f8df6017dda46e87728ade0fe54f3bb
nikasrmz <nikasharmaza@gmail.com>
2025-08-05 17:28:52 +0400

    small refactoring, opcode is now attribute, since it was parameter passed to majority of functions

--- core/cpu.py
+++ core/cpu.py
@@ -15,6 +15,7 @@ class CPU:
     i: int
     stack: List[int]
     sp: int
+    opcode: int
 
     def __init__(self):
         self.memory = Memory()
@@ -26,127 +27,124 @@ class CPU:
         self.stack = [0] * STACK_SIZE
         self.sp = 0
 
-    def high_nibble_dispatch(self, opcode: int):
+    def high_nibble_dispatch(self):
         """
         This function acts as a top level categorization for opcodes. Codes 0x3??? 
         through 0x7??? are grouped together, since those codes are for skipping lines,
         and altering register with outside values.
-
-        Parameters:
-            opcode: code to be dispatched
         """
-        match opcode & 0xF000:
+        match self.opcode & 0xF000:
             case 0x0000:
-                self.dispatch_sys_control(opcode)
+                self.dispatch_sys_control()
             case 0x1000 | 0xB000:
-                self.jump(opcode)
+                self.jump()
             case 0x2000:
-                self.call(opcode)
+                self.call()
             case 0x3000 | 0x4000 | 0x5000 | 0x9000:
-                self.dispatch_comparison(opcode)
+                self.dispatch_comparison()
             case 0x6000:
-                self.set_reg(opcode)
+                self.set_reg()
             case 0x7000:
-                self.add_nn_no_carry(opcode)
+                self.add_nn_no_carry()
             case 0x8000:
-                self.dispatch_reg_arithmetic(opcode)
+                self.dispatch_reg_arithmetic()
             case 0xA000:
-                self.set_i(opcode)
+                self.set_i()
             case 0xC000:
-                self.set_random_byte(opcode)
+                self.set_random_byte()
             case 0xD000:
-                self.draw_sprite(opcode)
+                self.draw_sprite()
             case 0xE000:
                 pass
             case 0xF000:
                 pass
             case _:
-                raise ValueError(f"Code {opcode} not supported.")
+                raise ValueError(f"Code {self.opcode} not supported.")
 
-    def _second_nibble(self, opcode: int):
-        return (opcode & 0x0F00) >> 8
+    def _second_nibble(self):
+        return (self.opcode & 0x0F00) >> 8
     
-    def _third_nibble(self, opcode: int):
-        return (opcode & 0x00F0) >> 4
+    def _third_nibble(self):
+        return (self.opcode & 0x00F0) >> 4
     
-    def _fourth_nibble(self, opcode: int):
-        return opcode & 0x000F
+    def _fourth_nibble(self):
+        return self.opcode & 0x000F
     
-    def _second_byte(self, opcode: int):
-        return opcode & 0x00FF
+    def _second_byte(self):
+        return self.opcode & 0x00FF
     
-    def _last_3_nibbles(self, opcode: int):
-        return opcode & 0x0FFF
+    def _last_3_nibbles(self):
+        return self.opcode & 0x0FFF
     
-    def dispatch_sys_control(self, opcode: int):
-        match opcode & 0x0FFF:
+    def dispatch_sys_control(self):
+        match self.opcode & 0x0FFF:
             case 0x00E0:
                 self.display.clear_screen()
             case 0x00EE:
                 self.return_from_subroutine()
             case _:
-                raise ValueError(f"Code {opcode} not supported.")
+                raise ValueError(f"Code {self.opcode} not supported.")
             
     def return_from_subroutine(self):
         self.sp -= 1
         self.pc = self.stack[self.sp]
 
-    def jump(self, opcode: int):
-        destination = opcode & 0x0FFF
-        match opcode & 0xF000:
+    def jump(self):
+        destination = self.opcode & 0x0FFF
+        match self.opcode & 0xF000:
             case 0x1000:
                 self.pc = destination
             case 0xB000:
                 self.pc = destination + self.registers[0]
             case _:
-                raise ValueError(f"Code {opcode} not supported.")
+                raise ValueError(f"Code {self.opcode} not supported.")
         self.pc_modified = True
 
-    def call(self, opcode: int):
+    def call(self):
         self.stack[self.sp] = self.pc
         self.sp += 1
-        self.pc = opcode & 0x0FFF
+        self.pc = self.opcode & 0x0FFF
         self.pc_modified = True
 
-    def dispatch_comparison(self, opcode: int):
-        match opcode & 0xF000:
+    def dispatch_comparison(self):
+        match self.opcode & 0xF000:
             case 0x3000 | 0x4000:
-                self.skip_eq_neq_nn(opcode)
+                self.skip_eq_neq_nn(self.opcode)
             case 0x5000 | 0x9000:
-                self.skip_eq_neq_reg(opcode)
+                self.skip_eq_neq_reg(self.opcode)
     
-    def skip_eq_neq_nn(self, opcode: int):
-        reg_value = self.registers[self._second_nibble(opcode)]
-        value = self._second_byte(opcode)
+    def skip_eq_neq_nn(self):
+        reg_value = self.registers[self._second_nibble(self.opcode)]
+        value = self._second_byte(self.opcode)
         if (
-            (opcode & 0xF000 == 0x3000 and value == reg_value) or 
-            (opcode & 0xF000 == 0x4000 and value != reg_value)
+            (self.opcode & 0xF000 == 0x3000 and value == reg_value) or 
+            (self.opcode & 0xF000 == 0x4000 and value != reg_value)
         ):
             self.pc += 2
 
-    def skip_eq_neq_reg(self, opcode: int):
-        reg1_value = self.registers[self._second_nibble(opcode)]
-        reg2_value = self.registers[self._third_nibble(opcode)]
+    def skip_eq_neq_reg(self):
+        reg1_value = self.registers[self._second_nibble(self.opcode)]
+        reg2_value = self.registers[self._third_nibble(self.opcode)]
         if (
-            (opcode & 0xF00F == 0x5000 and reg1_value == reg2_value) or 
-            (opcode & 0xF00F == 0x9000 and reg1_value != reg2_value)
+            (self.opcode & 0xF00F == 0x5000 and reg1_value == reg2_value) or 
+            (self.opcode & 0xF00F == 0x9000 and reg1_value != reg2_value)
         ):
             self.pc += 2
 
-    def set_reg(self, opcode: int):
-        reg_idx = self._second_nibble(opcode)
-        self.registers[reg_idx] = self._second_byte(opcode)
+    def set_reg(self):
+        reg_idx = self._second_nibble(self.opcode)
+        self.registers[reg_idx] = self._second_byte(self.opcode)
 
-    def add_nn_no_carry(self, opcode: int):
-        reg_idx = self._second_nibble(opcode)
-        value = self._second_byte(opcode)
+    def add_nn_no_carry(self):
+        reg_idx = self._second_nibble(self.opcode)
+        value = self._second_byte(self.opcode)
         self.registers[reg_idx] = (self.registers[reg_idx] + value) % 256
 
-    def dispatch_reg_arithmetic(self, opcode: int):
-        reg1_idx = self._second_nibble(opcode)
+    def dispatch_reg_arithmetic(self):
+        reg1_idx = self._second_nibble(self.opcode)
         reg1_value = self.registers[reg1_idx]
-        reg2_value = self.registers[self._third_nibble(opcode)]
-        match opcode & 0x000F:
+        reg2_value = self.registers[self._third_nibble(self.opcode)]
+        match self.opcode & 0x000F:
             case 0x0000:
                 self.registers[reg1_idx] = reg2_value
             case 0x0001:
@@ -166,7 +164,7 @@ class CPU:
             case 0x000E:
                 self.shift_reg_left(reg1_idx, reg1_value)
             case _:
-                ValueError(f"Code {opcode} not supported.")
+                ValueError(f"Code {self.opcode} not supported.")
 
     def add_reg_carry(self, reg_idx: int, value1: int, value2: int):
         sum_ = value1 + value2
@@ -186,18 +184,18 @@ class CPU:
         self.registers[-1] = value1 & 0b1000_0000
         self.registers[reg_idx] = (value1 & 0b0111_1111) << 1
 
-    def set_i(self, opcode: int):
-        self.i = self._last_3_nibbles(opcode)
+    def set_i(self):
+        self.i = self._last_3_nibbles(self.opcode)
 
-    def set_random_byte(self, opcode: int):
-        reg_idx = self._second_nibble(opcode)
-        value = self._second_byte(opcode)
+    def set_random_byte(self):
+        reg_idx = self._second_nibble(self.opcode)
+        value = self._second_byte(self.opcode)
         self.registers[reg_idx] = value & randint(0, 255)
 
-    def draw_sprite(self, opcode: int):
-        x = self._second_nibble(opcode)
-        y = self._third_nibble(opcode)
-        size = self._fourth_nibble(opcode)
+    def draw_sprite(self):
+        x = self._second_nibble(self.opcode)
+        y = self._third_nibble(self.opcode)
+        size = self._fourth_nibble(self.opcode)
         byte_array = [self.memory.read_byte(self.i + j) for j in range(size)]
         collision = self.display.draw_sprite(x, y, byte_array)
         self.registers[-1] = collision
