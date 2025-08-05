dffdec1ed9dbb299deeaf451301cac28939db06f
nikasrmz <nikasharmaza@gmail.com>
2025-08-05 10:38:45 +0400

    added implementation for comparison codes, added handy inner methods

--- core/cpu.py
+++ core/cpu.py
@@ -9,6 +9,7 @@ class CPU:
     display: Display
     registers: List[int]
     pc: int
+    pc_modified: bool
     i: int
     stack: List[int]
     sp: int
@@ -18,6 +19,7 @@ class CPU:
         self.display = Display()
         self.registers = [0] * REGISTER_COUNT
         self.pc = ROM_START_IDX
+        self.pc_modified = False
         self.i = 0
         self.stack = [0] * STACK_SIZE
         self.sp = 0
@@ -33,19 +35,19 @@ class CPU:
         """
         match opcode & 0xF000:
             case 0x0000:
-                self.sys_control(opcode)
+                self.dispatch_sys_control(opcode)
             case 0x1000 | 0xB000:
                 self.jump(opcode)
             case 0x2000:
                 self.call(opcode)
-            case 0x3000 | 0x4000 | 0x5000 | 0x6000 | 0x7000:
+            case 0x3000 | 0x4000 | 0x5000 | 0x9000:
+                self.dispatch_comparison(opcode)
+            case 0x6000 | 0x7000:
                 pass
             case 0x8000:
                 pass
-            case 0x9000:
-                pass
             case 0xA000:
-                pass
+                self.set_i(opcode)
             case 0xC000:
                 pass
             case 0xD000:
@@ -55,16 +57,31 @@ class CPU:
             case 0xF000:
                 pass
             case _:
-                raise NotImplementedError(f"Code {opcode} not supported.")
+                raise ValueError(f"Code {opcode} not supported.")
 
-    def sys_control(self, opcode: int):
+    def _second_nibble(self, opcode: int):
+        return (opcode & 0x0F00) >> 8
+    
+    def _third_nibble(self, opcode: int):
+        return (opcode & 0x00F0) >> 4
+    
+    def _fourth_nibble(self, opcode: int):
+        return opcode & 0x000F
+    
+    def _second_byte(self, opcode: int):
+        return opcode & 0x00FF
+    
+    def _last_3_nibbles(self, opcode: int):
+        return opcode & 0x0FFF
+    
+    def dispatch_sys_control(self, opcode: int):
         match opcode & 0x0FFF:
             case 0x00E0:
                 self.display.clear_screen()
             case 0x00EE:
                 self.return_from_subroutine()
             case _:
-                raise NotImplementedError(f"Code {opcode} not supported.")
+                raise ValueError(f"Code {opcode} not supported.")
             
     def return_from_subroutine(self):
         self.sp -= 1
@@ -78,11 +95,40 @@ class CPU:
             case 0xB000:
                 self.pc = destination + self.registers[0]
             case _:
-                raise 
+                raise ValueError(f"Code {opcode} not supported.")
+        self.pc_modified = True
 
     def call(self, opcode: int):
         self.stack[self.sp] = self.pc
         self.sp += 1
         self.pc = opcode & 0x0FFF
+        self.pc_modified = True
 
-        
+    def dispatch_comparison(self, opcode: int):
+        match opcode & 0xF000:
+            case 0x3000 | 0x4000:
+                self.skip_eq_neq_nn(opcode)
+            case 0x5000 | 0x9000:
+                self.skip_eq_neq_reg(opcode)
+    
+    def skip_eq_neq_nn(self, opcode: int):
+        reg_value = self.registers[self._second_nibble(opcode)]
+        value = self._second_byte(opcode)
+        if (
+            (opcode & 0xF000 == 0x3000 and value == reg_value) or 
+            (opcode & 0xF000 == 0x4000 and value != reg_value)
+        ):
+            self.pc += 2
+
+    def skip_eq_neq_reg(self, opcode: int):
+        reg1_value = self.registers[self._second_nibble(opcode)]
+        reg2_value = self.registers[self._third_nibble(opcode)]
+        if (
+            (opcode & 0xF00F == 0x5000 and reg1_value == reg2_value) or 
+            (opcode & 0xF00F == 0x9000 and reg1_value != reg2_value)
+        ):
+            self.pc += 2
+
+    def set_i(self, opcode: int):
+        self.i = self._last_3_nibbles(opcode)
+    
