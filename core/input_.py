9a11515d7eb4f5efda8cc94114c59158f332202e
nikasrmz <nikasharmaza@gmail.com>
2025-08-06 14:52:11 +0400

    feat(input) add empty input functionality for reference

--- core/input_.py
+++ core/input_.py
@@ -1,2 +1,12 @@
-class Input_:
-    pass
+class Input_: 
+    def __init__(self):
+        pass # TODO
+
+    def key_pressed(self, key: int):
+        pass # TODO
+
+    def key_not_pressed(self, key: int):
+        pass # TODO
+
+    def wait_store_key(self, key: int):
+        pass # TODO
